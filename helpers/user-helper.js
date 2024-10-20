5ba55526a03d52df5b1f64456171bb7c19cb533a
THOMAS V VARGHESE <THOMAS V VARGHESE@thoma.(none)>
2024-10-21 00:07:52 +0530

    fourth commit

--- helpers/user-helper.js
+++ helpers/user-helper.js
@@ -1,6 +1,5 @@
 var db = require("../config/connection");
 var collection = require("../config/collections");
-const bcrypt = require("bcrypt");
 
 var objectId = require("mongodb").ObjectId;
 const Razorpay = require("razorpay");
