8fd089a820a88eaf6cc58e2a0d83e114fd88e0b6
THOMAS V VARGHESE <THOMAS V VARGHESE@thoma.(none)>
2024-10-21 12:40:34 +0530

    sixth commit

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ const nocache = require('nocache');
 var http = require('http');
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 
 var server = http.createServer(app);
 server.listen(port);
--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
-const mongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 const state = { db: null };
 
-module.exports.connect = function(done) {
+module.exports.connect = async function(done) {
     const url = process.env.MONGO_URL || 'mongodb://localhost:27017';
     const dbname = 'shopping';
 
-    const client = new mongoClient(url);
-    client.connect()
-        .then(() => {
-            state.db = client.db(dbname);
-            done();
-        })
-        .catch((err) => done(err));
+    try {
+        const client = new MongoClient(url);
+        await client.connect();
+        state.db = client.db(dbname);
+        done();
+    } catch (err) {
+        done(err);
+    }
 };
 
 module.exports.get = function() {
