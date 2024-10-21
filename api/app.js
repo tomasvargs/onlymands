020f177dd89385f67ed7ab4ce81e22f4f059b8ff
THOMAS V VARGHESE <THOMAS V VARGHESE@thoma.(none)>
2024-10-21 13:13:09 +0530

    Update vercel.json for deployment

--- api/app.js
+++ api/app.js
@@ -4,12 +4,12 @@ var path = require('path');
 const bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var userRouter = require('./routes/user');
-var adminRouter = require('./routes/admin');
+var userRouter = require('../routes/user');
+var adminRouter = require('../routes/admin');
 var hbs = require('express-handlebars');
 var app = express();
 var fileUpload = require('express-fileupload');
-var db = require('./config/connection');
+var db = require('../config/connection');
 var session = require('express-session');
 const MongoStore = require('connect-mongo');
 const nocache = require('nocache');
@@ -91,16 +91,17 @@ app.post('/upload', upload.single('file'), (req, res) => {
 app.engine('hbs', hbs.engine({
   extname: 'hbs',
   defaultLayout: 'layout',
-  layoutsDir: __dirname + '/views/layout/',
-  partialsDir: __dirname + '/views/partials'
+  layoutsDir: path.join(__dirname, '../views/layout/'), // Updated path
+  partialsDir: path.join(__dirname, '../views/partials/') // Updated path
+
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views')); 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(fileUpload());
 
 db.connect((err) => {
